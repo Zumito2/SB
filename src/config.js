7c1bd771144a8a139aedfd828fa0444d245a7948
Zumito <victorzamorano94@gmail.com>
2025-03-14 16:46:44 +0100

    prueba 3

---
+++ src/config.js
@@ -0,0 +1,9 @@
+import {config} from 'dotenv'
+
+config()
+
+export const PORT = process.env.PORT
+export const DB_USER = process.env.PORT
+export const DB_PASSWORD = process.env.DB_PASSWORD
+export const DB_DATABASE = process.env.DB_DATABASE
+export const DB_PORT = process.env.DB_PORT
