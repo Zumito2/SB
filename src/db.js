f3f8111e4f3f9b9076a6e12f67b784db5a21546d
Zumito <victorzamorano94@gmail.com>
2025-03-14 13:49:48 +0100

    actualizacion db

--- src/db.js
+++ src/db.js
@@ -1,11 +1,13 @@
-import {createPool} from 'mysql2/promise'
+import { createPool } from 'mysql2/promise';
 
-export const pool = createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    port: 3306,
-    database: 'sbdb',
-    ssl: false
+// Obtener las variables de entorno
+const pool = createPool({
+    host: process.env.DB_HOST, // El host de la base de datos
+    user: process.env.DB_USER, // El usuario
+    password: process.env.DB_PASSWORD, // La contraseña
+    port: process.env.DB_PORT || 3306, // El puerto (opcional si es el predeterminado)
+    database: process.env.DB_NAME, // El nombre de la base de datos
+    ssl: process.env.DB_SSL === 'true' // Si está habilitado SSL (puede ser necesario en producción)
 });
 
+export { pool };
