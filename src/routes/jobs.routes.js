30bc1d2db59ae336b09f817a0a64b94148822d56
Betsaidars <betsaidars97@gmail.com>
2025-03-18 12:52:19 +0000

    cambios en el jobs.route

--- src/routes/jobs.routes.js
+++ src/routes/jobs.routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 
 // Importa los controladores de usuarios desde el archivo `users.controllers.js`.
 // Estos controladores son funciones que se encargan de manejar las solicitudes HTTP para los usuarios.
-import { getJobs, getJobsId, getJobsDate, getJob, startJob, endJob, getJobsFecha, createJob, updateJob, deleteJob} from '../controllers/jobs.controller.js';
+import { getJobs, getJobsId, getJobsDate, getJob, startJob, endJob, getJobsFecha, createJob, updateJob, deleteJob, getJobsByUser} from '../controllers/jobs.controller.js';
 import { authenticateToken } from '../authMiddleware.js'; // Importa el middleware
 
 // Crea una instancia de `Router` para definir las rutas de la API.
