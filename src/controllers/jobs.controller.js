2b69a7730bf84b4672b3303610da872c3e74b2b6
Zumito <victorzamorano94@gmail.com>
2025-03-28 11:55:51 +0100

    prueba

--- src/controllers/jobs.controller.js
+++ src/controllers/jobs.controller.js
@@ -64,18 +64,21 @@ export const getJobsFecha = async (req, res) => {
 };
 
 export const getJobsPendiente = async (req, res) => {
+  console.log("Buscando trabajos pendientes");
   try {
-    // Realiza una consulta SQL para obtener todos los trabajos pendientes.
-    const [rows] = await pool.query('SELECT * FROM jobs WHERE dateJob < now() AND (state = "Pendiente" OR state = "En Progreso")');
+    console.log("Realizando la consulta SQL...");
+    const [rows] = await pool.query(
+      `SELECT * FROM jobs WHERE dateJob < now() AND (state = "Pendiente" OR state = "En Progreso");`
+    );
 
-    // Responde con la lista de trabajos pendientes en formato JSON.
+    console.log("Resultados:", rows);
     res.json(rows);
   } catch (error) {
-    // Si ocurre un error en la consulta, se captura y se responde con un error 500 (Internal Server Error).
-    console.error("Error al ejecutar la consulta:", error); 
-    return res.status(500).json({ message: "Something goes wrong" });
+    console.error("Error al ejecutar la consulta:", error);
+    res.status(500).json({ message: "Something goes wrong" });
   }
 };
+
 
 
 export const getJobsDate = async (req, res) => {
